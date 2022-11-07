3abc66c7ab76dd6af8b84cd434a6d32526a91628
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-07 12:11:41 -0300

    feat: post courses data into db

---
+++ src/data/provisionalData.js
@@ -0,0 +1,22 @@
+export const provisionalData = {
+  courses:[
+            {
+              "id": 10,
+              "name": "curso1",
+              "subject": "fazendo funcionar",
+              "way": "dev"
+            },
+            {
+              "id": 22,
+              "name": "curso2",
+              "subject": "fazendo o pulguento funcionar",
+              "way": "dev"
+            },
+            {
+              "id": 33,
+              "name": "curso3",
+              "subject": "fazendo a pulga ir embora",
+              "way": "dev"
+            },
+        ]
+}
