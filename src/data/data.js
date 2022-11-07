bd8231247ebddc14f1bb6b80374a255e9e45d954
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-11-07 10:00:35 -0300

    chore: create first data file

---
+++ src/data/data.js
@@ -0,0 +1,141 @@
+export const data = {
+  "courses":[
+
+            {
+            "titulo": "Qual melhor linguagem de programação? | Com Wilian da Silva",
+            "road": "dev",
+            "tipo": "vídeo",
+            "author": "OrangeJuice",
+            "tempo": "51",
+            "tags": ["linguagem","programação", "código"],
+            "status": "done",
+            "link":"https://www.youtube.com/watch?v=zTMvQD5EtJw",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+
+            {
+            "titulo": "Habilidades além do código! | Com Mateus Oliveira",
+            "road": "dev",
+            "tipo": "vídeo",
+            "author": "OrangeJuice",
+            "tempo": "45",
+            "status": "progress",
+            "tags": ["softskill","comunicação", "habilidades"],
+            "link":"https://www.youtube.com/watch?v=Mmukepu3yRs",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+
+
+            {
+            "titulo": "Lógica de programação e algoritmos",
+            "road": "dev",
+            "tipo": "curso",
+            "author": "Udemy",
+            "tempo": "51",
+            "status": "unresolved",
+            "tags": ["logica","algoritmos", "curso"],
+            "link":"https://www.udemy.com/course/logica-de-programacao-e-algoritmos-iniciante/",
+              "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+
+            {
+            "titulo": "Básico HTML, CSS e Javascript",
+            "road": "dev",
+            "tipo": "curso",
+            "author": "Udemy",
+            "tempo": "131",
+            "status": "unresolved",
+            "tags": ["HTML","CSS", "JavaScript"],
+            "link":"https://www.udemy.com/course/criando-site-html-css-e-javascript/",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+             {
+            "titulo": "HTTP: Desmistificando o protocolo da Web",
+            "road": "dev",
+            "tipo": "artigo",
+            "author": "Alura",
+            "tempo": "45",
+            "status": "unresolved",
+            "tags": ["HTTP","internet", "Web"],
+            "link":"https://www.alura.com.br/artigos/desmistificando-o-protocolo-http-parte-1",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+
+              {
+            "titulo": "O que é uma API?",
+            "road": "dev",
+            "tipo": "artigo",
+            "author": "AWS",
+            "tempo": "180",
+            "status": "unresolved",
+            "tags": ["API","REST", "Cloud"],
+            "link":"https://aws.amazon.com/pt/what-is/api/",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+              {
+            "titulo": "Introdução a Bancos de Dados e Linguagem SQL",
+            "road": "dev",
+            "tipo": "curso",
+            "author": "Udemy",
+            "tempo": "120",
+            "status": "unresolved",
+            "tags": ["SQL","banco", "dados"],
+            "link":"https://www.udemy.com/course/introducao-a-bancos-de-dados-e-linguagem-sql/",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+               {
+            "titulo": "Instalar o Linux no Windows com o WSL",
+            "road": "dev",
+            "tipo": "artigo",
+            "author": "Windows",
+            "tempo": "20",
+            "tags": ["Linux","WSL", "Windows"],
+            "status": "unresolved",
+            "link":"https://learn.microsoft.com/pt-br/windows/wsl/install",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+
+            {
+            "titulo": "Versionamento com Git",
+            "road": "dev",
+            "tipo": "video",
+            "author": "OrangeJuice",
+            "tempo": "63",
+            "tags": ["Git","código", "GitFlow"],
+            "status": "unresolved",
+            "link":"https://www.youtube.com/watch?v=9k_lIYuRtg8",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              },
+            {
+            "titulo": "Git e Github para iniciantes",
+            "road": "dev",
+            "tipo": "curso",
+            "author": "Udemy",
+            "tempo": "125",
+            "tags": ["Git","código", "GitFlow"],
+            "status": "unresolved",
+            "link":"https://www.udemy.com/course/git-e-github-para-iniciantes/",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vulputate purus ut tincidunt posuere. In quis justo quam."
+              }
+
+
+
+
+
+
+
+
+  ]
+
+
+
+
+
+}
